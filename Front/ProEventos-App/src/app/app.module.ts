38e7f31f701db259a5718fb0d497e9e9c23d88c1
Paulo Eleison <paulo.eleison@gmail.com>
2023-02-22 21:53:54 -0300

    #Seção 5 Angular- Intepolação Diretivas e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import {HttpClientModule} from '@angular/common/http';// IMPORTANDO O MODULO HTTP PARA REQUISIÇÕES;
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //Importado para o uso do ngx-boostrap e bootstrap
+import { NavComponent } from './nav/nav.component';
+import  {CollapseModule} from 'ngx-bootstrap/collapse';// PARA O USO DO NAVBAR-COLLAPSE
+import {FormsModule} from '@angular/forms' // IMPORTADO PARA O USO DO FORMSMODULE PARA TO-WAY DATA-BIND
+// (ASSOCIAÇÃO EM DUAS LINHAS)
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    EventosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    HttpClientModule, // ARQUIVO DE CONFIGURAÇÃO CENTRAL DO PROJETO CORRESPONDENTE (HTTPCLIENT)
+    BrowserAnimationsModule, // ARQUIVO DE CONFIGURAÇÃO CENTRAL DO PROJETO CORRESPONDENTE(NGX-BOOSTRAP)
+    CollapseModule.forRoot(), // trazido como um modulo
+     FormsModule // ARQUIVO DE CONFIGURAÇÃO CENTRAL DO PROJETO CORRESPONDENTE
   ],
   providers: [],
   bootstrap: [AppComponent] // STARTA O PROJETO UTILIZANDO APPCOMPONENT POIS ISSO ELE PODE SER INSERIDO NA INDEX
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-eventos',
@@ -7,22 +7,61 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
- public eventos : any ;
-  constructor(private http : HttpClient) {
-   }
+  public eventos: any = [];
+  public eventos_filtrado : any = [];
+  public larguraimagem: number = 150; // px
+  public margem_imagem: number = 2; // px
+  public ocultarimagem: boolean = false; //
+  private _filtro_lista: string = null; // variavel para o filtro criada
+
+  constructor(private http: HttpClient) {
+
+
+  }
 
   ngOnInit(): void {
     this.GetEventos();
+
   }
 
-  public GetEventos() : void {
-     // SUBSCRIBE NÓS TEMOS QUE NOS INSCREVER
+  public get filtro_lista(){
+     return this._filtro_lista;
+  }
+
+  public set  filtro_lista(filtrado : string)
+  {
+    this._filtro_lista =  filtrado;
+    this.eventos_filtrado=  this._filtro_lista != null ? this.filtrar_lista(this.filtro_lista): this.eventos;
+  }
+  public GetEventos(): void {
+    // SUBSCRIBE NÓS TEMOS QUE NOS INSCREVER
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-           response=>this.eventos=response ,
-           error=>console.error(error)
+      response => {this.eventos = response;this.eventos_filtrado = response;}, // METODO PARA ATRIBUIR O MESMO RESULTADO PARA DUAS PROPRIEDADES
+      error => console.error(error),
+
 
     );
 
+  }
+
+  public filtrar_lista(conteudo : string): any {
+
+     return  this.eventos.filter(
+          evento=> evento.tema.includes(conteudo) || evento.local.includes(conteudo)
+
+     );
+
+
+  }
+
+
+
+
+
+
+
+public AlterarOcultarImagem() : void {
+  this.ocultarimagem = !this.ocultarimagem;
 }
 
 
