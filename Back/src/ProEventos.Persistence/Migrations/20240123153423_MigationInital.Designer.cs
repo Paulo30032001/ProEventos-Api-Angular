dc1b4753c357ba4055cb732743018d65edaba6c4
Paulo30032001 <paulo.eleison@gmail.com>
2024-01-31 19:00:07 -0300

    Retomando curso ajustes

--- Back/src/ProEventos.Persistence/Migrations/20240123153423_MigationInital.Designer.cs
+++ Back/src/ProEventos.Persistence/Migrations/20240123153423_MigationInital.Designer.cs
@@ -9,8 +9,8 @@ using ProEventos.Persistence.Contextos;
 namespace ProEventos.Persistence.Migrations
 {
     [DbContext(typeof(ProEventosContext))]
-    [Migration("20230414011309_Inicial")]
-    partial class Inicial
+    [Migration("20240123153423_MigationInital")]
+    partial class MigationInital
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -184,11 +184,13 @@ namespace ProEventos.Persistence.Migrations
                 {
                     b.HasOne("ProEventos.Domain.Evento", "Evento")
                         .WithMany("RedesSociais")
-                        .HasForeignKey("EventoId");
+                        .HasForeignKey("EventoId")
+                        .OnDelete(DeleteBehavior.Cascade);
 
                     b.HasOne("ProEventos.Domain.Palestrante", "Palestrante")
                         .WithMany("RedesSociais")
-                        .HasForeignKey("PalestranteId");
+                        .HasForeignKey("PalestranteId")
+                        .OnDelete(DeleteBehavior.Cascade);
 
                     b.Navigation("Evento");
 
